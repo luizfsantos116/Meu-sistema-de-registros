dfdb342cf0402f5772dc3191ad60db8440e49c8e
LuizFernando <luiz.f.santos116@aluno.senai.br>
2026-04-13 16:51:44 -0300

    Sistema de registro com o desafio final

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ console.log(caminhoArquivo); */
 const path = require('path');
 const caminhoArquivo = path.join(__dirname, 'atendimento.txt');
 fs.writeFileSync(caminhoArquivo, 'Cliente: Ana | Serviço: Troca de teclado');
-console.log('Arquivo criado!');
- */
+console.log('Arquivo criado!'); */
+
 // 04. Lendo o conteúdo do arquivo
 /* const fs = require('fs');
 const path = require('path');
@@ -21,9 +21,42 @@ const caminhoArquivo = path.join(__dirname, 'atendimento.txt');
 const conteudo = fs.readFileSync(caminhoArquivo, 'utf-8');
 console.log(conteudo); */
 
+// 05.  Vamos testar o que acontece ao gravar novamente
+/* const fs = require('fs');
+const path = require('path');
+const caminhoArquivo = path.join(__dirname, 'atendimento.txt');
+fs.writeFileSync(caminhoArquivo, 'Cliente: Bruno | Serviço: Impressora não funciona');
+console.log('Novo registro salvo!'); */
+
 // 06. Adicionando sem apagar
-const fs = require('fs');
+/* const fs = require('fs');
 const path = require('path');
 const caminhoArquivo = path.join(__dirname, 'atendimento.txt');
 fs.appendFileSync(caminhoArquivo, '\nCliente: Luiz | Serviço: Tela quebrada');
-console.log('Registro adicionado!');
+console.log('Registro adicionado!'); */
+
+// 07. Organizando melhor o sistema
+/* const fs = require('fs');
+const path = require('path');
+const caminhoArquivo = path.join(__dirname, 'registros', 'atendimento.txt');
+fs.writeFileSync(caminhoArquivo, 'Cliente: César | Serviço: Formatação do sistema');
+console.log('Arquivo salvo na pasta registros!');
+*/
+
+// 08. Visualizando o histórico
+/* const fs = require('fs');
+const path = require('path');
+const caminhoArquivo = path.join(__dirname, 'registros', 'atendimento.txt');
+const conteudo = fs.readFileSync(caminhoArquivo, 'utf-8');
+console.log('Histórico')
+console.log(conteudo); */
+
+// DESAFIO FINAL
+/* const fs = require('fs');
+const path = require('path');
+const caminhoArquivo = path.join(__dirname, 'registros', 'atendimento.txt');
+fs.appendFileSync(caminhoArquivo, '\nCliente: Felipe | Serviço: Driver');
+fs.appendFileSync(caminhoArquivo, '\nCliente: Gabriela | Serviço: Backup');
+fs.appendFileSync(caminhoArquivo, '\nCliente: Hugo | Serviço: Rede');
+console.log('Novos registros adicionados!');
+ */
