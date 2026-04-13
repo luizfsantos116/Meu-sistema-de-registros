affe9706d8848a8167d99b823b5d7ef7030e650e
LuizFernando <luiz.f.santos116@aluno.senai.br>
2026-04-13 16:45:44 -0300

    04. Lendo o conteúdo do arquivo

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ const caminhoArquivo = path.join(__dirname, 'atendimento.txt');
 console.log(caminhoArquivo); */
 
 // 03. Criando o primeiro arquivo com fs
-const fs = require('fs');
+/* const fs = require('fs');
 const path = require('path');
 const caminhoArquivo = path.join(__dirname, 'atendimento.txt');
 fs.writeFileSync(caminhoArquivo, 'Cliente: Ana | Serviço: Troca de teclado');
-console.log('Arquivo criado!');
+console.log('Arquivo criado!'); */
+
+// 04. Lendo o conteúdo do arquivo
+const fs = require('fs');
+const path = require('path');
+const caminhoArquivo = path.join(__dirname, 'atendimento.txt');
+const conteudo = fs.readFileSync(caminhoArquivo, 'utf-8');
+console.log(conteudo);
